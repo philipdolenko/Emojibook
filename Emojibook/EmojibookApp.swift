6460a76389ffaee24afc1cc33a6172bb9e9b9fc5
Philip Dolenko <philipdolenko@gmail.com>
2021-02-02 18:26:55 +0200

    Default SwiftUI project

---
+++ Emojibook/EmojibookApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojibookApp.swift
+//  Emojibook
+//
+//  Created by Philip Dolenko on 02.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojibookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
